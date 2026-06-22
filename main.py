9d8892512210c245d8839cb476812962e06792b6
Dhruvi Srivastava <dhruvisrivastava27@gmail.com>
2026-06-23 00:44:19 +0530

    Day 1 completed - Resume upload and parsing

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, UploadFile, File, Form
+import os
+
+from utils.parser import (
+    extract_pdf_text,
+    extract_docx_text
+)
+
+
+
+app = FastAPI()
+
+@app.post("/job-description")
+async def job_description(
+    jd: str = Form(...)
+):
+    return {
+        "job_description": jd
+    }
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Resume Parser API Running"
+    }
+
+@app.post("/upload-resume")
+async def upload_resume(
+    file: UploadFile = File(...)
+):
+
+    file_path = os.path.join(
+        UPLOAD_DIR,
+        file.filename
+    )
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    if file.filename.endswith(".pdf"):
+        text = extract_pdf_text(file_path)
+
+    elif file.filename.endswith(".docx"):
+        text = extract_docx_text(file_path)
+
+    else:
+        return {
+            "error": "Unsupported file format"
+        }
+
+    return {
+        "filename": file.filename,
+        "extracted_text": text[:3000]
+    }
---
+++ utils/parser.py
@@ -0,0 +1,24 @@
+import fitz
+from docx import Document
+
+
+def extract_pdf_text(file_path):
+    text = ""
+
+    pdf = fitz.open(file_path)
+
+    for page in pdf:
+        text += page.get_text()
+
+    return text
+
+
+def extract_docx_text(file_path):
+    doc = Document(file_path)
+
+    text = "\n".join(
+        paragraph.text
+        for paragraph in doc.paragraphs
+    )
+
+    return text
